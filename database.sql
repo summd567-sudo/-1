48545071a3324620e4b044948151b1e58c2aeb3b
summd567-sudo <su.mmd567@gmail.com>
2025-11-28 22:36:33 +0300

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+  id INT PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255)
+);
+
+INSERT INTO users VALUES
+(1, 'Ahmad', 'ahmad@example.com'),
+(2, 'Sarah', 'sarah@example.com');
